7d3dbdd5f6a3267baae0a8907d5beef9f71d04ad
Shivanand vishwakarma <vshivanand469@gmail.com>
2026-08-02 12:13:18 +0530

    Update Navbar CSS - responsive sidebar for mobile

--- src/components/home/Hero/Hero.jsx
+++ src/components/home/Hero/Hero.jsx
@@ -1,70 +1,46 @@
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Hammer } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 import styles from "./Hero.module.css";
 
 import Button from "../../ui/Button/Button";
 
-
 const Hero = () => {
+  const navigate = useNavigate();
 
   return (
-
     <section className={styles.hero}>
-
-
       <div className={styles.overlay}></div>
 
-
-
       <div className={styles.content}>
-
-
         <p className={styles.small}>
+          <Hammer size={14} />
           Welcome to Shiva Build Mart
         </p>
 
-
-
         <h1>
-          Build Your Dream Home
-          With Quality Materials
+          Build Your Dream Home With{" "}
+          <span className={styles.highlight}>Quality Materials</span>
         </h1>
 
-
-
         <p className={styles.desc}>
-          Premium plywood, hardware, construction
-          materials and interior solutions under one roof.
+          Premium plywood, hardware, construction materials and interior
+          solutions under one roof.
         </p>
 
-
-
         <div className={styles.actions}>
-
-
-          <Button>
+          <Button onClick={() => navigate("/shop")}>
             Shop Now
-            <ArrowRight size={18}/>
+            <ArrowRight size={18} />
           </Button>
 
-
-
-          <Button variant="secondary">
+          <Button variant="secondary" onClick={() => navigate("/shop")}>
             Explore Products
           </Button>
-
-
         </div>
-
-
       </div>
-
-
     </section>
-
   );
-
 };
-
 
 export default Hero;

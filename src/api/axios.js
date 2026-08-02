c21d83caeb7e7e90b3ade29062c53d8fe9ea9150
Shivanand vishwakarma <vshivanand469@gmail.com>
2026-08-02 07:46:45 +0530

    Fix backend API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 
-  baseURL: "https://shiva-build-mart-api.onrender.com/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 
   headers: {
     "Content-Type": "application/json",
